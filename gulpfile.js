22043a5223edc51490bfbaf52a947a4084a0d686
vijayst <vijayst@gmail.com>
2016-07-11 20:51:28 +0530

    Moving node_modules within the app.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 
-gulp.task('build', ['buildJs', 'copyHtml', 'copyPublic']);
+gulp.task('build', ['buildJs', 'copyHtml', 'copyPublic', 'copyModules']);
 
-gulp.task('buildJs', () => {
-  return gulp.src('app/**/*.js')
+gulp.task('buildJs', ['buildLib', 'buildModels', 'buildIndex']);
+
+gulp.task('buildModels', () => {
+  return gulp.src('app/models/**/*.js')
+  .pipe(babel({presets: ['es2015']}))
+  .pipe(gulp.dest('build/models'));
+});
+
+gulp.task('buildLib', () => {
+  return gulp.src('app/lib/**/*.js')
+  .pipe(babel({presets: ['es2015']}))
+  .pipe(gulp.dest('build/lib'));
+});
+
+gulp.task('buildIndex', () => {
+  return gulp.src('app/index.js')
   .pipe(babel({presets: ['es2015']}))
   .pipe(gulp.dest('build'));
 });
@@ -17,4 +31,9 @@ gulp.task('copyHtml', () => {
 gulp.task('copyPublic', () => {
   return gulp.src('app/public/**/*')
   .pipe(gulp.dest('build/public'));
+});
+
+gulp.task('copyModules', () => {
+  return gulp.src('app/node_modules/**/*')
+  .pipe(gulp.dest('build/node_modules'));
 });
