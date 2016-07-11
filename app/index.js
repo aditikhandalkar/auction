8618454d6a153b702c1c872df5ca1530632897da
vijayst <vijayst@gmail.com>
2016-07-11 20:28:12 +0530

    serving static files using express.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,14 @@
-console.log('app');
+import express from 'express';
+
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  console.log(__dirname);
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.listen(9000, () => {
+  console.log('listening on port 9000');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 
-gulp.task('build', () => {
+gulp.task('build', ['buildJs', 'copyHtml', 'copyPublic']);
+
+gulp.task('buildJs', () => {
   return gulp.src('app/**/*.js')
-  .pipe(babel({ presets: ['es2015'] }))
+  .pipe(babel({presets: ['es2015']}))
   .pipe(gulp.dest('build'));
+});
+
+gulp.task('copyHtml', () => {
+  return gulp.src('app/**/*.html')
+  .pipe(gulp.dest('build'));
+});
+
+gulp.task('copyPublic', () => {
+  return gulp.src('app/public/**/*')
+  .pipe(gulp.dest('build/public'));
 });
