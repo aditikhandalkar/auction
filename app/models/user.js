795c69ead41fdfcd1336ea35a848756b08923a05
vijayst <vijayst@gmail.com>
2016-07-11 11:26:18 +0530

    added more user tests.

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,9 @@
 export default class User {
   constructor(props) {
     this.name = props.name || '';
+    this.coins = 1000;
+    this.breads = 30;
+    this.carrots = 18;
+    this.diamonds = 1;
   }
 }
--- test/user_spec.js
+++ test/user_spec.js
@@ -16,6 +16,18 @@ describe('models', () => {
     it('name is correct', () => {
       user.name.should.equal('vijay');
     });
+    it('coins default to 1000', () => {
+      user.coins.should.equal(1000);
+    });
+    it('breads default to 30', () => {
+      user.breads.should.equal(30);
+    });
+    it('carrots default to 18', () => {
+      user.carrots.should.equal(18);
+    });
+    it('diamonds default to 1', () => {
+      user.diamonds.should.equal(1);
+    });
     it('has no password', () => {
       should.not.exist(user.password);
     });
