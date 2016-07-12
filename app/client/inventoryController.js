5f9fd2c8e028e7fb0297ffef93b3bb100456a837
vijayst <vijayst@gmail.com>
2016-07-12 10:13:11 +0530

    inventory controller and layout.

---
+++ app/client/inventoryController.js
@@ -0,0 +1,21 @@
+app.controller('InventoryController', function($scope, $rootScope) {
+  $scope.items = [];
+  $scope.$on('login', function(e, args) {
+    console.log('received event');
+    $scope.items.push({
+      image: '/images/bread_icon.jpg',
+      name: 'bread',
+      value: args.userInfo.breads
+    });
+    $scope.items.push({
+      image: '/images/carrot_icon.png',
+      name: 'carrot',
+      value: args.userInfo.carrots
+    });
+    $scope.items.push({
+      image: '/images/diamond_icon.png',
+      name: 'diamond',
+      value: args.userInfo.diamonds
+    });
+  });
+});
